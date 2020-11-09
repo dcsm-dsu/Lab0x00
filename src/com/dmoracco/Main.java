e2a7c99cbca678c14d6f6f1c089fd694e54f61f4
dcsm-dsu <david.moracco@trojans.dsu.edu>
2020-11-09 14:32:44 -0600

    Refactored output with double ratios

--- src/com/dmoracco/Main.java
+++ src/com/dmoracco/Main.java
@@ -43,16 +43,24 @@ public class Main {
         long temp = 0;
 
         boolean loopOne = true, loopTwo = true, loopThree = true;
-        long lastNL1 = maxTime, lastNL2 = maxTime, lastNL3 = maxTime;
+        long lastNL1 = 1, lastNL2 = 1, lastNL3 = 1;
+        long previousNL1 = 0, previousNL2 = 0, previousNL3 = 0 ;
 
-        String nl1Out, nl2Out, nl3Out;
-
-        System.out.printf("\n\nN\t\t\t\tSingle Loop\t\t\t\t\tDouble Loop\t\t\t\t\tTriple Loop\n");
+        String nl1Out, nl2Out, nl3Out, nl1Ratio = "na", nl2Ratio = "na", nl3Ratio = "na";
+;
+        System.out.printf("\n\n %16s%-32s     %-32s     %-32s\nN", "", "Single Loop", "Double Loop", "Triple Loop");
+        System.out.printf("%16s%-14s%-8s%-8s", "", "Time", "Ratio", "Ex.Ratio");
+        System.out.printf("       %-14s%-8s%-8s", "Time", "Ratio", "Ex.Ratio");
+        System.out.printf("       %-14s%-8s%-8s", "Time", "Ratio", "Ex.Ratio\n");
         for (int ITERATION_SIZE = 1; ITERATION_SIZE < MAX_SIZE; ITERATION_SIZE = ITERATION_SIZE*2 ){
 
             if (ITERATION_SIZE < 0) break;
 
             // Single Nested Loop
+            if (lastNL1 >= maxTime){
+                loopOne = false;
+                NL1 = -1;
+            }
             if (loopOne){
                 timeStampBefore = getCpuTime();
                 for (int i = 1; i < ITERATION_SIZE; i++){
@@ -61,14 +69,15 @@ public class Main {
                 timeStampAfter = getCpuTime();
 
                 NL1 = (timeStampAfter - timeStampBefore)/1000;
+                previousNL1 = lastNL1;
                 lastNL1 = NL1;
-            }
-            if (lastNL1 >= maxTime){
-                loopOne = false;
-                NL1 = -1;
             }
 
             // Double Nested Loop
+            if (lastNL2 >= maxTime){
+                loopTwo = false;
+                NL2 = -1;
+            }
             if (loopTwo){
 
                 timeStampBefore = getCpuTime();
@@ -80,14 +89,15 @@ public class Main {
                 timeStampAfter = getCpuTime();
 
                 NL2 = (timeStampAfter - timeStampBefore)/1000;
+                previousNL2 = lastNL2;
                 lastNL2 = NL2;
-            }
-            if (lastNL2 >= maxTime){
-                loopTwo = false;
-                NL2 = -1;
             }
 
             // Triple Nested Loop
+            if (lastNL3 >= maxTime){
+                loopThree = false;
+                NL3 = -1;
+            }
             if (loopThree){
 
                 timeStampBefore = getCpuTime();
@@ -101,11 +111,8 @@ public class Main {
                 timeStampAfter = getCpuTime();
 
                 NL3 = (timeStampAfter - timeStampBefore)/1000;
+                previousNL3 = lastNL3;
                 lastNL3 = NL3;
-            }
-            if (lastNL3 >= maxTime){
-                loopThree = false;
-                NL3 = -1;
             }
 
             // NL formatting
@@ -124,9 +131,30 @@ public class Main {
             } else {
                 nl3Out = Long.toString(NL3);
             }
+            //Ratio Formatting
+            if (ITERATION_SIZE > 1){
+                if (NL1 > -1 && previousNL1 != 0){
+                    nl1Ratio = String.format("%2.2f", (double)lastNL1/previousNL1);
+                } else {
+                    nl1Ratio = "na";
+                }
+                if (NL2 > -1 && previousNL2 != 0){
+                    nl2Ratio = String.format("%2.2f", ((double)lastNL2/previousNL2));
+                } else {
+                    nl2Ratio = "na";
+                }
+                if (NL3 > -1 && previousNL3 != 0){
+                    nl3Ratio = String.format("%2.2f", ((double)lastNL3/previousNL3));
+                } else {
+                    nl3Ratio = "na";
+                }
+            }
 
 
-            System.out.printf("%-8d\t\t%-15s\t\t\t\t%-15s\t\t\t\t%-15s\n", ITERATION_SIZE, nl1Out, nl2Out, nl3Out);
+            System.out.printf("%-10d%7s%-14s%-8s%-10s     %-14s%-8s%-10s     %-14s%-8s%-10s\n", ITERATION_SIZE,"",
+                    nl1Out, nl1Ratio, "~2" ,
+                    nl2Out, nl2Ratio, "~4" ,
+                    nl3Out, nl3Ratio, "~8");
         }
 
     }
